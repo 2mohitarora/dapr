66b124023828fe956cfda0d0777c552eaa2501bf
Mohit Arora <mohit.arora0@walmart.com>
2026-04-05 22:22:23 -0400

    Fixed registry scripts

--- kubernetes/vind-with-cilium-mesh/cluster-2/cluster-2-script.sh
+++ kubernetes/vind-with-cilium-mesh/cluster-2/cluster-2-script.sh
@@ -1,9 +1,9 @@
 for node in vcluster.cp.cluster-2 vcluster.node.cluster-2.worker-1; do
-  docker exec "$node" mkdir -p /etc/containerd/certs.d/registry:5000
-  docker exec "$node" sh -c 'cat > /etc/containerd/certs.d/registry:5000/hosts.toml << EOF
-server = "http://registry:5000"
+  docker exec "$node" mkdir -p /etc/containerd/certs.d/registry-2:5000
+  docker exec "$node" sh -c 'cat > /etc/containerd/certs.d/registry-2:5000/hosts.toml << EOF
+server = "http://registry-2:5000"
 
-[host."http://registry:5000"]
+[host."http://registry-2:5000"]
   capabilities = ["pull", "resolve"]
   skip_verify = true
 EOF'
